3c788ff17c6dd36069e2f8b12733024b2351fc55
wellyda-maar <wellydamaria9@gmail.com>
2026-05-11 19:08:38 -0300

    Deletado

--- Questao3Exercicio01.java
+++
@@ -1,9 +0,0 @@
-public class Questao3Exercicio01 {
-     public static void main(String[] args) {
-        int i = 3;
-        while(i<=100){
-        while(i%3==0){
-        }
-        }    
-    }      
-}
