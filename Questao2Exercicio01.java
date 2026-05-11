b39faae0aa4fd925a90f4352f8e3e9ddc6f9a158
wellyda-maar <wellydamaria9@gmail.com>
2026-05-11 19:08:58 -0300

    Deletado

--- Questao2Exercicio01.java
+++
@@ -1,9 +0,0 @@
-public class Questao2Exercicio01 {
-     public static void main(String[] args) {
-        int soma = 0;
-            for (int i = 0; i <= 1000; i++) {
-                soma += i;
-        }
-        System.out.println("A soma dos numeros de 0 a 1000 é:" + soma);
-    }
-}
