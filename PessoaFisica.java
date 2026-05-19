660d9fd0ba644d64b5c35dd2d4bdcb17ecd6c0f4
wellyda-maar <wellydamaria9@gmail.com>
2026-05-18 22:08:12 -0300

    Create PessoaFisica.java

---
+++ PessoaFisica.java
@@ -0,0 +1,22 @@
+public class PessoaFisica extends Pessoaa {
+
+    private String cpf;
+    private char sexo;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+}
+
