b8d6b5187768b362284109de61dc4a0b73d56d01
wellyda-maar <wellydamaria9@gmail.com>
2026-05-18 22:08:41 -0300

    Create PessoaJuridica.java

---
+++ PessoaJuridica.java
@@ -0,0 +1,13 @@
+public class PessoaJuridica extends Pessoaa {
+
+    private String cnpj;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
+
