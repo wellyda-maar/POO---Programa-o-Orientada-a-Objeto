8a684a54fffb6168471a5be9ee81cd08a205efc9
wellyda-maar <wellydamaria9@gmail.com>
2026-05-26 22:22:54 -0300

    Create Autor.java

---
+++ Autor.java
@@ -0,0 +1,12 @@
+public class Autor {
+    private String nome;
+
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
